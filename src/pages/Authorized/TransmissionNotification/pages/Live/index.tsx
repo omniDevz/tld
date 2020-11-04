1dafeed96189ea99283787b8f3f980434af0f594
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2020-11-04 14:36:20 -0300

    integrate live

--- src/pages/Authorized/TransmissionNotification/pages/Live/index.tsx
+++ src/pages/Authorized/TransmissionNotification/pages/Live/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { useToasts } from 'react-toast-notifications';
 
@@ -7,14 +7,22 @@ import PageAuthorized from '../../../../../components/PageAuthorized';
 
 import api from '../../../../../services/api';
 
-import { Live, ButtonsWrapper } from './styled';
+import { Live, ButtonsWrapper, Description, Frame } from './styled';
 
-import { IParamsTransmissionNotification } from './interface';
+import {
+  IParamsTransmissionNotification,
+  ITransmissionNotificationApi,
+  ITransmissionNotification,
+} from './interface';
 
 const TransmissionNotificationLive: React.FC = () => {
-  let { transmissionNotificationId } = useParams<
-    IParamsTransmissionNotification
-  >();
+  const [live, setLive] = useState<ITransmissionNotification>(
+    {} as ITransmissionNotification
+  );
+
+  const {
+    transmissionNotificationId,
+  } = useParams() as IParamsTransmissionNotification;
   const { addToast } = useToasts();
   const history = useHistory();
 
@@ -45,10 +53,45 @@ const TransmissionNotificationLive: React.FC = () => {
       });
   }
 
+  function handleGetLive() {
+    api
+      .get(`notificacaoTransmissao/${transmissionNotificationId}`)
+      .then((response) => {
+        if (response.status === 206) {
+          addToast(response.data, {
+            appearance: 'warning',
+            autoDismiss: true,
+          });
+          return;
+        }
+
+        const liveApi = response.data as ITransmissionNotificationApi;
+
+        setLive({
+          description: liveApi.descricao,
+          TransmissionNotificationId: liveApi.notificacaoTransmissaoId,
+          link: liveApi.link,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        addToast(
+          'Houve algum erro inesperado na busca por live, tente novamente mais tarde',
+          {
+            appearance: 'error',
+            autoDismiss: true,
+          }
+        );
+      });
+  }
+
+  useEffect(handleGetLive, []);
+
   return (
     <PageAuthorized type="back" text="Ao vivo">
       <Live>
-        <img src="https://source.unsplash.com/random/person" alt="img" />
+        <Description>{live.description}</Description>
+        <Frame src={live.link} />
       </Live>
       <ButtonsWrapper>
         <Button color="primary-outline" onClick={handleDeleteLive}>
--- src/pages/Authorized/TransmissionNotification/pages/Live/styled.ts
+++ src/pages/Authorized/TransmissionNotification/pages/Live/styled.ts
@@ -9,17 +9,37 @@ export const Live = styled.div`
   flex: 1;
   border: 2px solid ${(props) => props.theme.colors.secondary};
   max-height: calc(100vh - 27rem);
-
-  img {
-    object-fit: cover;
-    max-height: 100%;
-    max-width: 100%;
-    overflow: hidden;
-  }
+  position: relative;
+  overflow: hidden;
 `;
 
 export const ButtonsWrapper = styled.div`
   margin: 2.4rem 0;
   display: flex;
   column-gap: 1.6rem;
+`;
+
+export const Frame = styled.iframe`
+  position: absolute;
+  left: 50%;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  border: none;
+  transform: translateX(-50%);
+`;
+
+export const Description = styled.h4`
+  padding: .8rem 24px;
+  font-size: 2rem;
+  text-align: center;
+  position: absolute;
+  left: -1px;
+  right: -1px;
+  bottom: -2px;
+  background: linear-gradient(0deg, black -17%, transparent 100%);
+  z-index: 1000;
+  color: ${(props) => props.theme.colors.tertiary};
 `;
