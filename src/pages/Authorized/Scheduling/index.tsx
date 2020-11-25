04212b580da3b3a626f26b532661f92172dc0a26
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2020-11-25 02:27:53 -0300

    template calendar

--- src/pages/Authorized/Scheduling/index.tsx
+++ src/pages/Authorized/Scheduling/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import PageAuthorized from '../../../components/PageAuthorized';
@@ -9,19 +9,131 @@ import util from '../../../utils/util';
 
 import {
   SchedulingWrapper,
+  CalendarWrapper,
   ButtonWrapper,
+  CloseCalendar,
+  DaysOfTheWeek,
+  DayOfTheWeek,
+  CalendarBody,
+  YearWrapper,
   MoreToday,
+  BackMount,
+  NextMount,
   ListDays,
+  Calendar,
   Header,
+  Mount,
   Today,
 } from './styled';
 
 const Scheduling: React.FC = () => {
+  const [days, setDays] = useState<
+    {
+      days: number[];
+    }[]
+  >([]);
+  const [openCalendar, setOpenCalendar] = useState(false);
+
+  function handleGetDaysInMonth(month: number = 0, year: number = 0) {
+    const daysInMonth = (iMonth: number = 0, iYear: number = 0) => {
+      return 32 - new Date(iYear, iMonth, 32).getDate();
+    };
+
+    let firstDay = new Date(year, month).getDay();
+
+    setDays([]);
+
+    let weeksDays: {
+      days: number[];
+    }[] = [];
+    let date = 1;
+    for (let i = 0; i < 6; i++) {
+      let weekDay: {
+        days: number[];
+      } = {
+        days: [],
+      };
+
+      for (let j = 0; j < 7; j++) {
+        if (i === 0 && j < firstDay) {
+          weekDay.days.push(-1);
+        } else if (date > daysInMonth(month, year)) {
+          break;
+        } else {
+          weekDay.days.push(date);
+
+          date++;
+        }
+      }
+
+      weeksDays.push(weekDay);
+    }
+
+    setDays([...weeksDays]);
+  }
+
+  useEffect(handleGetDaysInMonth, []);
+
+  function handleGetByDay(day: number) {
+    console.log(day);
+  }
+
+  function handleCalendar() {
+    setOpenCalendar(!openCalendar);
+  }
+
   return (
     <PageAuthorized type="back" text="Agendamentos" footer={false}>
       <ButtonWrapper>
-        <Button color="secondary-outline">Abrir calendário</Button>
+        {!openCalendar && (
+          <Button color="secondary-outline" onClick={handleCalendar}>
+            Abrir calendário
+          </Button>
+        )}
       </ButtonWrapper>
+      {!!openCalendar && (
+        <CalendarWrapper>
+          <YearWrapper>
+            2020 <CloseCalendar onClick={handleCalendar} />
+          </YearWrapper>
+          <Calendar>
+            <Mount>
+              <BackMount />
+              Junho
+              <NextMount />
+            </Mount>
+            <DaysOfTheWeek>
+              <DayOfTheWeek>D</DayOfTheWeek>
+              <DayOfTheWeek>S</DayOfTheWeek>
+              <DayOfTheWeek>T</DayOfTheWeek>
+              <DayOfTheWeek>Q</DayOfTheWeek>
+              <DayOfTheWeek>Q</DayOfTheWeek>
+              <DayOfTheWeek>S</DayOfTheWeek>
+              <DayOfTheWeek>S</DayOfTheWeek>
+            </DaysOfTheWeek>
+            <CalendarBody>
+              <tbody>
+                {!!days.length &&
+                  days.map((day) => (
+                    <tr key={`tr_${day.days[0]}`}>
+                      {!!day.days &&
+                        day.days.map((d) =>
+                          d === -1 ? (
+                            <td key={d} />
+                          ) : (
+                            <td onClick={() => handleGetByDay(d)} key={d}>
+                              {d}
+                            </td>
+                          )
+                        )}
+                    </tr>
+                  ))}
+              </tbody>
+            </CalendarBody>
+          </Calendar>
+        </CalendarWrapper>
+      )}
+
       <SchedulingWrapper>
         <Header>
           <Today>{util.getFormatDateNameMountNow()}</Today>
--- src/pages/Authorized/Scheduling/styled.ts
+++ src/pages/Authorized/Scheduling/styled.ts
@@ -1,5 +1,95 @@
-import { FiX } from 'react-icons/fi';
+import { FiArrowLeft, FiArrowRight, FiX } from 'react-icons/fi';
 import styled from 'styled-components';
+
+export const CalendarWrapper = styled.div`
+  padding: 2.4rem 0;
+`;
+
+export const YearWrapper = styled.p`
+  font-size: 3.2rem;
+  font-weight: 500;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  grid-gap: .8rem;
+  margin-bottom: 1.2rem;
+`;
+
+export const CloseCalendar = styled(FiX)`
+  border: 2.5px solid ${(props) => props.theme.colors.secondary};
+  height: 4rem;
+  width: 4rem;
+  cursor: pointer;
+  transform: scale(1);
+  transition: all 260ms ease-in-out;
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(.92);
+  }
+`;
+
+export const Calendar = styled.div`
+  border: 2.5px solid ${(props) => props.theme.colors.secondary};
+  padding: 1.2rem 2.4rem;
+`;
+
+export const BackMount = styled(FiArrowLeft)`
+  width: 4rem;
+  height: 4rem;
+  cursor: pointer;
+`;
+export const NextMount = styled(FiArrowRight)`
+  width: 4rem;
+  height: 4rem;
+  cursor: pointer;
+`;
+
+export const Mount = styled.p`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 3.2rem;
+  padding-bottom: 1.6rem;
+`;
+
+export const DaysOfTheWeek = styled.div`
+  display: grid;
+    grid-template-columns: repeat(7, 1fr);
+  border-bottom: 3px solid ${(props) => props.theme.colors.secondary};
+  padding: .8rem 2.4rem;
+`;
+
+export const DayOfTheWeek = styled.label`
+  font-size: 2.4rem;
+  font-weight: 500;
+  text-align: center;
+`;
+
+export const CalendarBody = styled.table`
+  padding: 1.2rem 2.4rem 0;
+  display: flex;
+  flex-direction: column;
+
+  tr {
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    justify-content: space-between;
+    padding: .8rem 0;
+
+    td {
+      text-align: center;
+      font-size: 2.4rem;
+      transform: scale(1);
+      transition: all 260ms ease-in-out;
+      cursor: pointer;
+
+      &:hover {
+        transform: scale(1.33);
+      }
+    }
+  }
+`;
 
 export const ButtonWrapper = styled.div`
   margin-top: 2rem;
@@ -8,7 +98,7 @@ export const ButtonWrapper = styled.div`
 
 export const SchedulingWrapper = styled.div`
   background: ${(props) => props.theme.colors.secondary};
-  height: calc(100vh - 15.5rem);
+  min-height: calc(100vh - 15.5rem);
   margin-top: 2rem;
   padding: 3.2rem;
 `;
